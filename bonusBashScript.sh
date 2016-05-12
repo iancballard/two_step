38b3957c3ccdcb7f9aa76a8b067f7f22059d86ab
iancballard <iancballard@gmail.com>
2016-05-12 10:29:48 -0700

    pay the pilot mturk subjects their bonuses

---
+++ bonusBashScript.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+pushd /Applications/aws-mturk-clt-1.3.1/bin
+./grantBonus.sh -workerid A2VU8AC7MY3721 -amount 2.42 -assignment 30OG32W0SUBIJJYQ8APX158A8CMNEX -reason "Your bonus for performing my hit." 
+./grantBonus.sh -workerid A37S96RT1P1IT2 -amount 2.5 -assignment 31QTRG6Q2TDGS9RUOQEJCX5LQ3SPYA -reason "Your bonus for performing my hit." 
+popd
